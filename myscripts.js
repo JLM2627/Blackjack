a81bb6e2281056e49bc523adddf90c92debe4466
JLM2627 <arcal619@gmail.com>
2017-09-13 15:41:33 -0400

    code cleanup in js

--- myscripts.js
+++ myscripts.js
@@ -1,30 +1,21 @@
 $(() => {
     //js document ready
-    //console.log()
+    
 const cardSuites = ['hearts', 'clubs', 'spades' , 'diamonds'];
-// array of all four suites
 const cardValues = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-// array of values for card(listed as strings? strings vs strings and integers)
-
-const deck = [];// empty array for my newCard to be stored in
+const deck = [];
 const playerHand =[];
 const dealerHand =[];
 
 function createDeck () {
     for(var x = 0; x < cardSuites.length; x++) { // suit loop that will run four times
       for(var y = 0; y < cardValues.length; y++) {
-            // this cardValue loop will run 13 times
-            // 13 individual cardValues * 4 cardSuites = 52 cards
             var card = {
                 suite: cardSuites[x],
                 value: cardValues[y]
-                // once newCard is created using cardSuites and cardValues, starting at index [0]
             };
-                // for this array, we are going to push my newCard into deck
              deck.push(card);
-        
-
-      }
+        }
     }
     return deck;
 };
@@ -39,9 +30,6 @@ function deal()
         console.log(playerHand)
         dealerHand.push(deck[Math.floor(Math.random() * deck.length)]);//generates random card between 0 and 52
         console.log(dealerHand)
-        
-        
-
     }
 };
 
