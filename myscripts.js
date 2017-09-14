d7d7a6d8035f8959b35c5ef4e431ac85e3e530b5
JLM2627 <arcal619@gmail.com>
2017-09-14 13:35:07 -0400

    dealOneCard working for player

--- myscripts.js
+++ myscripts.js
@@ -1,42 +1,86 @@
 $(() => {
     //js document ready
-    
-const cardSuites = ['hearts', 'clubs', 'spades' , 'diamonds'];
-const cardValues = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-const deck = [];
-const playerHand =[];
-const dealerHand =[];
 
-    function createDeck () {
-        let worth = 0;
-        for(var x = 0; x < cardSuites.length; x++) { // suit loop that will run four times
-            for(var y = 0; y < cardValues.length; y++) {
-                let worth = parseInt(cardValues[x]);
-                if(cardValues[x] === "J" || cardValues[x] === "Q" || cardValues[x] === "K")
+    const cardSuites = ['hearts', 'clubs', 'spades', 'diamonds'];
+    const cardValues = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    const deck = [];
+    const playerHand = [];
+    const dealerHand = [];
+    //const card =[];
+
+    function createDeck() {
+        // let worth = 0;
+        for (var x = 0; x < cardSuites.length; x++) { // suit loop that will run four times
+            for (var y = 0; y < cardValues.length; y++) {
+                const cardValue = cardValues[y];
+                let worth = parseInt(cardValue);
+                if (cardValue === "J" || cardValue === "Q" || cardValue === "K") {
                     worth = 10;
-                if(cardValues[x] === "A")
-                   worth = 11;
+                }
+
+                if (cardValue === "A") {
+                    worth = 11;
+                }
+
+
+                let card = {
+                    suit: cardSuites[x],
+                    value: cardValue,
+                    worth: worth
+                }
+                deck.push(card);
+
 
             }
-                var card = {suite: cardSuites[x], value: cardValues[y], worth: worth };
-             deck.push(card);
-            }  
-            return deck; 
-};
-    console.log(createDeck());
+
+        }
+        // var card = {suite: cardSuites[x], value: cardValues[y], worth: worth };
+        // deck.push(card);
+        return deck;
+    }
 
 
-    function deal()
-    {
+    function deal() {
         for (var i = 0; i < 2; i++)//
         {
-        playerHand.push(deck[Math.floor(Math.random() * deck.length)]);//generates random card between 0 and 52
+            playerHand.push(deck[Math.floor(Math.random() * deck.length)]);//generates random card between 0 and 52
+            dealerHand.push(deck[Math.floor(Math.random() * deck.length)]);//generates random card between 0 and 52
+        }
         console.log(playerHand)
-        dealerHand.push(deck[Math.floor(Math.random() * deck.length)]);//generates random card between 0 and 52
         console.log(dealerHand)
-    }
-};
+        firstHandResult();
+    };
 
- deal();
-});
+    function firstHandResult() {
+        if (playerHand === 21 && dealerHand === 21) {
+            console.log("tie");
+        } else if (dealerHand === 21) {
+            console.log("Dealer wins!");
+        } else if (playerHand === 21) {
+            console.log("You Win!");
+        }
+    }
+
+    $("#dealBtn").on("click", function () {
+        console.log("i work");
+        createDeck();
+        deal();
+    })
+
+
+    function dealOneCard(){
+            playerHand.push(deck[Math.floor(Math.random() * deck.length)]);
+            console.log(playerHand)
+            console.log(dealerHand)
+    }
+
+    $("#hitBtn").on("click", function () {
+        console.log("i work");
+        dealOneCard();
+       
+    })
+
+
+
+    });
 //You could use jQuery for events. User clicks on card => run function
