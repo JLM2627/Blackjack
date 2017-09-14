60811cf0060ad883c88beb31c0d2cb79c5113823
JLM2627 <arcal619@gmail.com>
2017-09-14 08:23:43 -0400

    random save

--- myscripts.js
+++ myscripts.js
@@ -7,25 +7,25 @@ const deck = [];
 const playerHand =[];
 const dealerHand =[];
 
-function createDeck () {
-    for(var x = 0; x < cardSuites.length; x++) { // suit loop that will run four times
-      for(var y = 0; y < cardValues.length; y++) {
-            var card = {
+    function createDeck () {
+        for(var x = 0; x < cardSuites.length; x++) { // suit loop that will run four times
+            for(var y = 0; y < cardValues.length; y++) {
+                var card = {
                 suite: cardSuites[x],
                 value: cardValues[y]
-            };
+                };
              deck.push(card);
-        }
-    }
+            }   
+        }   
     return deck;
 };
-console.log(createDeck());
+    console.log(createDeck());
 
 
-function deal()
-{
-    for (var i = 0; i < 2; i++)//per 1000 runs
+    function deal()
     {
+        for (var i = 0; i < 2; i++)//
+        {
         playerHand.push(deck[Math.floor(Math.random() * deck.length)]);//generates random card between 0 and 52
         console.log(playerHand)
         dealerHand.push(deck[Math.floor(Math.random() * deck.length)]);//generates random card between 0 and 52
@@ -35,3 +35,4 @@ function deal()
 
  deal();
 });
+//You could use jQuery for events. User clicks on card => run function
