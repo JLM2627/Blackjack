ff3f476752d0769991faf755ea7b9f536cae8c22
JLM2627 <arcal619@gmail.com>
2017-09-15 09:17:15 -0400

    cards added

--- myscripts.js
+++ myscripts.js
@@ -6,28 +6,28 @@ $(() => {
     const deck = [];
     const playerHand = [];
     const dealerHand = [];
-   
-    
+
+
     let totalWorthP = 0;
     let totalWorthD = 0;
     const score = [];
 
 
 
-    
+
     function counterP() {
-        totalWorthP = playerHand.reduce(function(whatever, asif){
+        totalWorthP = playerHand.reduce(function (whatever, asif) {
             return whatever + asif.worth;
         }, 0)
         console.log(totalWorthP);
     }
     function counterD() {
-        totalWorthD = dealerHand.reduce(function(whatever, asif){
+        totalWorthD = dealerHand.reduce(function (whatever, asif) {
             return whatever + asif.worth;
         }, 0)
         console.log(totalWorthD);
     }
-    
+
     function createDeck() {
         for (var x = 0; x < cardSuites.length; x++) { // suit loop that will run four times
             for (var y = 0; y < cardValues.length; y++) {
@@ -40,7 +40,7 @@ $(() => {
                 if (cardValue === "A") {
                     worth = 11;
                 }
-                     let card = {
+                let card = {
                     suit: cardSuites[x],
                     value: cardValue,
                     worth: worth
@@ -55,52 +55,52 @@ $(() => {
     }
 
     function deal() {
-        for (var i = 0; i < 2; i++){
-             playerHand.push(deck[Math.floor(Math.random() * deck.length)]);//generates random card between 0 and 52
+        for (var i = 0; i < 2; i++) {
+            playerHand.push(deck[Math.floor(Math.random() * deck.length)]);//generates random card between 0 and 52
             dealerHand.push(deck[Math.floor(Math.random() * deck.length)]);//generates random card between 0 and 52
         }
         console.log(playerHand)//DO NOT DELETE THESE LINES OF CODE
-        console.log(dealerHand)   
+        console.log(dealerHand)
     }
-         
+
     $("#deal").on("click", function () {
         console.log("i work");
         createDeck();
         deal();
         counterP();
         counterD();
-        handResult();        
+        handResult();
     });
-    
-    
-    
 
-    
-    
-    function dealOneCard(){
-            playerHand.push(deck[Math.floor(Math.random() * deck.length)]);
-            
+
+
+
+
+
+    function dealOneCard() {
+        playerHand.push(deck[Math.floor(Math.random() * deck.length)]);
+
     }
 
     $("#hit").on("click", function () {
-      console.log("i work");
-      //deal(); 
-      dealOneCard();
+        console.log("i work");
+        //deal(); 
+        dealOneCard();
         counterP();
-       console.log(playerHand);
-       counterD();
-       console.log(dealerHand);
-       handResult();
+        console.log(playerHand);
+        counterD();
+        console.log(dealerHand);
+        handResult();
     });
 
 
-    function dealAnotherCard(){
-        
+    function dealAnotherCard() {
+
         dealerHand.push(deck[Math.floor(Math.random() * deck.length)]);
 
     }
 
-    
+
     function handResult() {
         if (totalWorthP === 21 && totalWorthD === 21) {
             console.log("tie");
@@ -112,25 +112,24 @@ $(() => {
             console.log("hit or stay player?");
         } else if (totalWorthD > 21) {
             console.log("dealer busts, player wins!")
-        } else if(totalWorthP > 21){
+        } else if (totalWorthP > 21) {
             console.log("You bust!")
-        } else if (totalWorthP < 21){
+        } else if (totalWorthP < 21) {
             console.log("You're under 21. Do you want hit!?")
         }
     }
-        
-        $("#stay").on("click", function (){
-           // deal();
-           //dealOneCard();
-           dealAnotherCard()
-            counterP();
-           console.log(playerHand);
-           counterD();
-           console.log(dealerHand);
-          
-           handResult();
 
-        })
+    $("#stay").on("click", function () {
+        // deal();
+        //dealOneCard();
+        dealAnotherCard()
+        counterP();
+        console.log(playerHand);
+        counterD();
+        console.log(dealerHand);
+
+        handResult();
+
+    })
 });
 
-      
